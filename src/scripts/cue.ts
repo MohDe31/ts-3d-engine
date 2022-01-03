70f99a28860bac6a5ce4d6b4ff3f881827bd3b67
unknown <bouderbalamohammed2000@gmail.com>
2022-01-03 16:36:31 +0100

    fix position change

--- src/scripts/cue.ts
+++ src/scripts/cue.ts
@@ -1,6 +1,6 @@
 import { Transform } from "../core/transform";
 import { Component } from "../core/component";
-import { vec2Magnitude, vec2SqrMagnitude, Vec3, vec3Normal, vec3xNumMulR, vec3xVec3AddR, vec3xVec3SubR } from "../utils/vecUtils";
+import { vec2Magnitude, vec2SqrMagnitude, Vec3, vec3Normal, vec3Set, vec3xNumMulR, vec3xVec3AddR, vec3xVec3SubR } from "../utils/vecUtils";
 import { Mouse } from "../core/mouse";
 import { RigidBody2D } from "../core/rigidbody";
 import { Time } from "../core/time";
@@ -52,7 +52,7 @@ export class Cue extends Component {
                 // Zoom in
                 const dir: Vec3 = vec3Normal(vec3xVec3SubR(this.camera.position, this.whiteBall.position));
 
-                this.camera.position = vec3xVec3AddR(vec3xNumMulR(dir, 10), this.whiteBall.position);
+                vec3Set(this.camera.position, vec3xVec3AddR(vec3xNumMulR(dir, 10), this.whiteBall.position));
                 
                 // Rotate around the ball
                 this.cameraLook.toggleAroundMode(this.whiteBall.position);
